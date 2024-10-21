6e289e81e5c44a941be7b5c7a33b2b548ea9cf85
MatisseBro <132852961+MatisseBro@users.noreply.github.com>
2024-10-21 10:04:10 +0200

    parfait

---
+++ loic.js
@@ -0,0 +1,20 @@
+function nombrePair(nombre) {
+    if ( nombre % 2 === 0 ) {
+        return "LE NOMBRE EST PAIR"
+    } else {
+        "LE NOMBRE EST IMPAIR LOOSER"
+    };
+}
+
+console.log(4);
+// Fonction pour inverser une chaîne de caractères
+function inverserChaine() {
+    // Récupérer la valeur de l'input
+    const inputString = document.getElementById("inputString").value;
+
+    // Inverser la chaîne en utilisant split, reverse et join
+    const reversedString = inputString.split('').reverse().join('');
+
+    // Afficher la chaîne inversée
+    document.getElementById("result").textContent = "Chaîne inversée: " + reversedString;
+}
